f47c6fcae9855828901e1ba4c2e456af05429398
electricat <phamtudu123@gmail.com>
2026-05-26 21:21:52 +0700

    First commit

---
+++ src/main/java/batdau/hocptpmud/Hocptpmud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package batdau.hocptpmud;
+
+/**
+ *
+ * @author loan phuong
+ */
+public class Hocptpmud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
